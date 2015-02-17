eda93d49706d6bd510b3ccd7f23dde61d59a634c
Marko Rizvic <marko.rizvic@gmail.com>
2015-02-17 12:35:48 +0100

    DHCP scripts

--- dhcp-discover-transcation.py
+++ dhcp-discover-transcation.py
@@ -3,29 +3,29 @@
 from scapy.all import *
 
 conf.checkIPaddr=False
+
+# configuration
 localiface = 'eth1'
 requestMAC = 'fc:4d:d4:33:2f:41'
-myhostname='sundancer'
+myhostname='vektor'
 localmac = get_if_hwaddr(localiface)
 localmacraw = requestMAC.replace(':','').decode('hex')
 
-
+# craft DHCP DISCOVER
 dhcp_discover = Ether(src=localmac, dst='ff:ff:ff:ff:ff:ff')/IP(src='0.0.0.0', dst='255.255.255.255')/UDP(dport=67, sport=68)/BOOTP(chaddr=localmacraw,xid=RandInt())/DHCP(options=[('message-type', 'discover'), 'end'])
-
 print dhcp_discover.display()
 
+# send discover, wait for reply
 dhcp_offer = srp1(dhcp_discover,iface=localiface)
-
 print dhcp_offer.display()
 
+# craft DHCP REQUEST from DHCP OFFER
 myip=dhcp_offer[BOOTP].yiaddr
 sip=dhcp_offer[BOOTP].siaddr
 xid=dhcp_offer[BOOTP].xid
-
 dhcp_request = Ether(src=localmac,dst="ff:ff:ff:ff:ff:ff")/IP(src="0.0.0.0",dst="255.255.255.255")/UDP(sport=68,dport=67)/BOOTP(chaddr=localmacraw,xid=xid)/DHCP(options=[("message-type","request"),("server_id",sip),("requested_addr",myip),("hostname",myhostname),("param_req_list","pad"),"end"])
-
 print dhcp_request.display()
 
+# send request, wait for ack
 dhcp_ack = srp1(dhcp_request,iface=localiface)
-
 print dhcp_ack.display()
--- dhcp-release.py
+++ dhcp-release.py
@@ -3,14 +3,14 @@
 from scapy.all import *
 
 conf.checkIPaddr=False
+
+#configuration
 localiface = 'eth1'
-requestMAC = 'fc:4d:d4:33:2f:41'
-requestMACraw = requestMAC.replace(':','').decode('hex')
+releaseMAC = 'fc:4d:d4:33:2f:41'
 releaseIP='192.168.179.113'
 serverIP='192.168.179.5'
-localmac = get_if_hwaddr(localiface)
-foo,localmacraw = get_if_raw_hwaddr(localiface)
+releaseMACraw = releaseMAC.replace(':','').decode('hex')
 
-dhcp_release = IP(dst=serverIP)/UDP(sport=68,dport=67)/BOOTP(chaddr=requestMACraw, ciaddr=releaseIP, xid=RandInt())/DHCP(options=[('message-type','release'), 'end'])
-
+#craft and send DHCP RELEASE 
+dhcp_release = IP(dst=serverIP)/UDP(sport=68,dport=67)/BOOTP(chaddr=releaseMACraw, ciaddr=releaseIP, xid=RandInt())/DHCP(options=[('message-type','release'), 'end'])
 send(dhcp_release)
